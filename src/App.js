4f008089ca7b688effb358f84b6fd5938840b365
penin21 <penin21@naver.com>
2024-06-26 18:01:38 +0900

    첫

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState } from 'react';
+import ImageResizer from './resizer'
+
+function App() {
+
+ 
+
+  return (
+    <div className="App">
+      <ImageResizer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/resizer.js
@@ -0,0 +1,110 @@
+import React, { useState } from 'react';
+import Resizer from 'react-image-file-resizer';
+import loadingImg from './loading.gif';
+
+const ImageResizer = () => {
+  const [resizedImages, setResizedImages] = useState([]);
+  const [alertMessage, setAlertMessage] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const resizeFile = (file) =>
+    new Promise((resolve, reject) => {
+      Resizer.imageFileResizer(
+        file,
+        35,
+        30,
+        'PNG',
+        100,
+        0,
+        (uri) => {
+          fetch(uri)
+            .then((res) => res.blob())
+            .then((blob) => {
+              resolve({ blob, uri, name: file.name });
+            })
+            .catch((error) => {
+              reject(error);
+            });
+        },
+        'base64'
+      );
+    });
+
+  const handleFileChange = async (event) => {
+    const files = Array.from(event.target.files);
+    const promises = files.map(file => resizeFile(file));
+
+    setLoading(true);
+
+    try {
+      const resizedImages = await Promise.all(promises);
+      setResizedImages(resizedImages);
+
+      // Download in batches of 10 images
+      for (let i = 0; i < resizedImages.length; i += 8) {
+        const batch = resizedImages.slice(i, i + 8);
+        
+        for (const { blob, name } of batch) {
+          const link = document.createElement('a');
+          const url = window.URL.createObjectURL(blob);
+          link.href = url;
+          link.download = name;
+          document.body.appendChild(link);
+          link.click();
+          document.body.removeChild(link);
+          window.URL.revokeObjectURL(url);
+
+          // Check if download was successful
+          if (!document.hasFocus()) {
+            setAlertMessage(`Failed to download ${name}`);
+          }
+        }
+
+        // Pause for 5 seconds between batches
+        if (i + 8 < resizedImages.length) {
+          await delay(5000); // 5 seconds delay
+        }
+      }
+    } catch (error) {
+      setAlertMessage('Failed to resize/upload one or more images.');
+    } finally {
+      setLoading(false);
+      alert('다운로드가 완료되었습니다.')
+    }
+  };
+
+  const delay = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  };
+
+  return (
+    <div style={{ marginLeft: '20px' }}>
+      {loading && <img src={loadingImg} alt="로딩 중..." />}
+      {alertMessage && <div className="alert alert-danger">{alertMessage}</div>}
+      <div style={{ textAlign: 'left' }}>
+        사용방법:<br />
+        1. 파일 선택 → 이미지 선택 (다중 선택 가능)<br />
+        2. 자동으로 다운로드 진행됨. 크롬에서 여러 파일 다운로드 '허용' <br />
+        3. 크롬에서는 동시 다운로드가 10개까지만 지원하므로, 9개 다운 + 5초 휴식 패턴으로 반복됨
+        <br />
+      </div>
+      <input type="file" multiple onChange={handleFileChange} />
+      {resizedImages.length > 0 && (
+        <div>
+          <h3>리사이즈된 이미지 (총: {resizedImages.length}개):</h3>
+          <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+            {resizedImages.map((image, index) => (
+              <div key={index} style={{ width: '33.33%', marginBottom: '20px', boxSizing: 'border-box', textAlign: 'center', display: 'flex', alignItems: 'center' }}>
+                <span style={{ fontSize: '18px', fontWeight: 'bold', paddingRight: '10px' }}>{index + 1}.</span>
+                <img src={image.uri} alt={`resized-${index}`} style={{ maxWidth: '100%', height: 'auto' }} />
+                <span style={{ paddingLeft: '10px' }}> {image.name}</span>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ImageResizer;
